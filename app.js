144c002373c7df9210a8a2db3b4230459a8cdc95
Musfirah Lodhi <Musfirahlodhi4@gmail.com>
2024-04-16 10:40:06 +0500

    a20

---
+++ app.js
@@ -0,0 +1,46 @@
+// // question pdf 31-40 
+// // q1
+// var dObj = new Date();
+// // q2
+// var date = new Date();
+// var string = date.toString();
+// var dStr = string;
+// // q3
+// var d = new Date();
+// var day = d.getDay();
+// // q4
+// var dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
+// alert(dayNames[day]);
+// // q5
+// var currentDate = new Date();
+// var year = currentDate.getFullYear();
+// var month = currentDate.getMonth() + 1;
+// var day = currentDate.getDate();
+// var hours = currentDate.getHours();
+// var minutes = currentDate.getMinutes();
+// var seconds = currentDate.getSeconds();
+// var milliseconds = currentDate.getMilliseconds();
+// var dayOfWeek = currentDate.getDay();
+
+// console.log("Year:", year);
+// console.log("Month:", month);
+// console.log("Day:", day);
+// console.log("Hours:", hours);
+// console.log("Minutes:", minutes);
+// console.log("Seconds:", seconds);
+// console.log("Milliseconds:", milliseconds);
+// console.log("Day of the Week:", dayOfWeek);
+// // q6
+// var lastDay = new Date(2020, 11, 0);
+// var daysName = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday" ];
+// var lDay = lastDay.getDay();
+// var lastDayName =daysName[lDay]
+// alert(lastDayName);
+// // q7
+// var secondDay = new Date(1992, 1, 2);
+// var daysName = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday" ];
+// var sDay = secondDay.getDay();
+// var secondDayName =daysName[sDay]
+// alert(secondDayName);
+
+
